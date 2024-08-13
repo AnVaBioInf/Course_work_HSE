6af10481cabb1a9b95867f20d9d5d982843e19b5
AnVaBioInf <96749066+AnVaBioInf@users.noreply.github.com>
2024-08-13 19:55:48 +0300

    Create uORF_prediction2.R

---
+++ uORF_prediction2.R
@@ -0,0 +1,5 @@
+# https://rdrr.io/bioc/IsoformSwitchAnalyzeR/man/analyzeCPAT.html
+analyzeCPAT(aSwitchList,
+            "/home/an/Downloads/CPAT-1.2.4/bin/output2",
+            codingCutoff = 0.725,
+            removeNoncodinORFs=T)
