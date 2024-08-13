6e2657983ca773a27328c9e800c2321b00568a6b
AnVaBioInf <96749066+AnVaBioInf@users.noreply.github.com>
2024-08-13 16:00:46 +0300

    Create set_variables.sh

---
+++ set_variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+human_genome_fasta=/NEW_HDD/HDD_3TB/avasileva/course_work/human_genome/gencode/GRCh37.p13.genome.fa
+human_genome_gtf=/NEW_HDD/HDD_3TB/avasileva/course_work/human_genome/gencode/gencode.v19.chr_patch_hapl_scaff.annotation.gtf
+programs=/NEW_HDD/HDD_3TB/avasileva/bin
+hek=/NEW_HDD/HDD_3TB/avasileva/course_work/HEK293
+
+files_id=('SRR1275413' 'SRR1275414' 'SRR1275415' 'SRR1275416')   # все имена файлов, которые нужно скачать
